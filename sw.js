e4bdf3406281033a1c89d96e2a3318aaff286ff1
Dr. Skonk <dr.skonk@gmail.com>
2026-09-19 13:35:41 +0200

    V4_4

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C="suivi-v3-1",A=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="suivi-sante-v4";const A=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
