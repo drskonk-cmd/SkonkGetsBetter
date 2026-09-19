f90f6bfc45883afec30a7c3becbe594d734f8b4c
Dr. Skonk <dr.skonk@gmail.com>
2026-09-19 16:04:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const C="suivi-sante-v4";const A=["./","./index.html","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+const C="suivi-sante-v4-5";
+const A=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
